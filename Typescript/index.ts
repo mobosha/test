aacb584c828023e041049728fbfe808375873fac
孙沙沙 <sunshasha@chinamobo.com>
2017-04-06 09:31:30 +0800

    task

---
+++ Typescript/index.ts
@@ -0,0 +1,14 @@
+enum Color {Red = 1, Green, Blue};
+var colorName: string = Color[2];
+
+alert(colorName);
+
+var list:any[] = [1, true, "free",'ss'];
+
+
+list[1] = 100;
+
+console.log("???");
+console.log("~~~!");
+
+
