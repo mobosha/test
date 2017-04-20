aee1dde490144d1e7c8f0e845902563c4f7ae7d9
孙沙沙 <sunshasha@chinamobo.com>
2017-04-20 10:34:45 +0800

    typescript

--- Typescript/app/app.ts/index.ts
+++ Typescript/app/app.ts/index.ts
@@ -1,7 +1,7 @@
 enum Color {Red = 1, Green, Blue};
 var colorName: string = Color[2];
 
-alert(colorName);
+//alert(colorName);
 
 var list:any[] = [1, true, "free",'ss'];
 
@@ -28,11 +28,11 @@ function createSquare(config: SquareConfig): {color: string; area: number} {
 }
 //--createSquare 返回的对象是 newSquare，所有只能获取 color 和 area 并获取不了 width这个属性的值..
 var mySquare1 = createSquare({color: "red"});//与接口的变量 color 一样，此时这个值是取出是默认值 color=red
-var mySquare2 = createSquare({color1: "red"});//与接口的变量 color 不一样，此时这个值是取出是默认值 color=white
+var mySquare2 = createSquare({color: "green"});//与接口的变量 color 不一样，此时这个值是取出是默认值 color=white
 console.log(mySquare1.color+"=="+mySquare1.area);//
 console.log(mySquare2.color+"=="+mySquare2.area);//
 var mySquare3 = createSquare({color: "yellow",width:80});//这里给了两个变量值，一个是 color，一个是 widthconsole.log(mySquare3.color+"=="+mySquare3.area);//所以这个值必须等于 8000
-
+console.log(mySquare3.color + "==" + mySquare3.area); //所以这个值必须等于 8000
 
 
 
