8633f070144adfbbf15312d118735a1c44655976
孙沙沙 <sunshasha@chinamobo.com>
2017-04-28 16:05:40 +0800

    解决冲突

--- Typescript/app/app.ts/fx.ts
+++ Typescript/app/app.ts/fx.ts
@@ -9,12 +9,9 @@ return arg;
 * 或者:我们可以描述使用“任意”类型的标识功能：
 */
 function identity2(arg: any): any {
-<<<<<<< HEAD
     
 return arg;
 }
 console.log("sss");
-=======
-return arg;
-}
->>>>>>> 17f8f1e18129d401f665c2ab247d360fe1c0d381
+
+
